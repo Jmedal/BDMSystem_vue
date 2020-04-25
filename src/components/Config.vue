c34d7e49c9cd2cbfbc6642ad4962213f25042289
Jmedal <784203082@qq.com>
2020-04-25 23:23:46 +0800

    2020-4-24 更新 未上线的Config模块

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -1,44 +1,21 @@
 <template>
   <div>
-    <el-table
-      ref="filterTable"
-      :data="tableData"
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="日期"
-        sortable
-        width="180"
-        column-key="date"
-        :filters="dateList"
-        :filter-method="filterHandler"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址"
-        :formatter="formatter">
-      </el-table-column>
-      <el-table-column
-        prop="tag"
-        label="标签"
-        width="100"
-        :filters="[{ text: '家', value: '家' }, { text: '公司', value: '公司' }]"
-        :filter-method="filterTag"
-        filter-placement="bottom-end">
-        <template slot-scope="scope">
-          <el-tag
-            :type="scope.row.tag === '家' ? 'primary' : 'success'"
-            disable-transitions>{{scope.row.tag}}
-          </el-tag>
-        </template>
-      </el-table-column>
-    </el-table>
+    <el-row :gutter="20" style="height:250px; background-color: white; border-radius: 3px; box-shadow: 0 0 10px #ddd;">
+      <el-col :span="1" style="height: 1px">
+      </el-col>
+      <el-col :span="20">
+        <el-steps :active="active" finish-status="success">
+          <el-step title="大一"></el-step>
+          <el-step title="大二"></el-step>
+          <el-step title="大三"></el-step>
+          <el-step title="大四"></el-step>
+          <el-step :title="msg[i]"></el-step>
+        </el-steps>
+        <el-button style="margin-top: 20px; float: left;" @click="next">升学</el-button>
+      </el-col>
+      <el-col :span="1" style="height: 1px">
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -47,48 +24,25 @@
     name: 'config',
     data () {
       return {
-        tableData: [],
-        dateList: []
+        msg: ['毕业', '退学'],
+        i: 0,
+        active: 0
       }
     },
     mounted () {
-      this.getData()
     },
     methods: {
-      getData () {
-        this.$axios.get('/api/test/tableList')
-          .then(res => {
-            if (res.data.code === 0 && res.data.data && res.data.data.length > 0) {
-              this.tableData = res.data.data
-              for (let i = 0; i < this.tableData.length; i++) {
-                this.dateList.push({text: this.tableData[i].date, value: this.tableData[i].date})
-              }
-            }
-            console.log(res)
-          }, error => {
-            console.log(error)
-          })
-      },
-      resetDateFilter () {
-        this.$refs.filterTable.clearFilter('date')
-      },
-      clearFilter () {
-        this.$refs.filterTable.clearFilter()
-      },
-      formatter (row, column) {
-        return row.address
-      },
-      filterTag (value, row) {
-        return row.tag === value
-      },
-      filterHandler (value, row, column) {
-        const property = column['property']
-        return row[property] === value
+      next () {
+        if (this.active++ > 4){
+          this.active = 0
+          this.i = 0
+        }
+        if (this.active === 5) this.i = 1
       }
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="less" scoped>
 </style>
